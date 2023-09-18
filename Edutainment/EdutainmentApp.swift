34a80b25957a54c4fd0901d1b4693007d3248770
ZachMommaerts <zmommaerts@gmail.com>
2023-09-17 20:23:47 -0500

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Zach Mommaerts on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
